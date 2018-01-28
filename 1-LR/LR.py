ef04770f4a1f1e95750f62034cb6bf29d3c1f4f3
sheldonwong <wxt0628@gmail.com>
2018-01-28 18:44:35 +0800

    add LR

--- 1-LR/LR.py
+++ 1-LR/LR.py
@@ -9,23 +9,31 @@ def loadDataset(filename):
     for line in fr.readlines():
         lineArr = line.strip().split()
         dataMat.append([1.0,float(lineArr[0]),float(lineArr[1])])
-        labelMat.append(int(lineArr[2]))
+        labelMat.append([int(lineArr[2])])
     return dataMat,labelMat
 
 def sigmoid(inX):
     return 1.0/(1+exp(-inX))
 
+'''
+批梯度下降，
+更新一次参数，需要用到所有数据点
+'''
 def gradAscent(dataMatIn,classLabels):
+    #100 * 3
     dataMatrix = mat(dataMatIn)
+    #100 * 1
     labelMat = mat(classLabels)
+    #100 3
     m,n = shape(dataMatrix)
     alpha = 0.001
     iter = 500
+    # 3 * 1
     weights = ones((n,1))
     for k in range(iter):
-        h = sigmoid(dataMatrix*weights)
-        error = (labelMat-h)
-        weights = weights + alpha * dataMatrix.transpose()*error
+        h = sigmoid(dot(dataMatrix,weights))
+        error = labelMat-h
+        weights = weights + alpha * dot(dataMatrix.T,error)
     return weights
 
 data,label = loadDataset('testSet.txt')
--- 1-LR/test.py
+++ 1-LR/test.py
@@ -5,14 +5,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #1. 载入数据
+data = []
+label = []
 fr = open('testSet.txt')
 for line in fr.readlines():
+    lineArr = line.strip().split()
+    data.append([1.0,float(lineArr[0]),float(lineArr[1])])
+    label.append(lineArr[2])
     
+data_df = pd.DataFrame(data)
+data_df[3] = label
+#选择列
+#data_df[1]
+
 
 #2. 可视化
 
+#可视化散点图，两种思路，
+#一种思路是每次在画布上画出同一个label的sample，
+#另外一种是每次画一个，在画的时候，判断颜色
+plt.figure()
+
+for index,row in data_df.iterrows():
+    x = row[1]
+    y = row[2]
+    c = row[3]
+    
+    if(c == '1'):
+        color = 'r'
+        marker = 'x'
+    else:
+        color = 'b'
+        marker = 'o'
+
+    plt.scatter(x,y,c=color,marker=marker)
+
+plt.show()
 
 #3. 初始化
+
+
 
 
 #4. 拟合
