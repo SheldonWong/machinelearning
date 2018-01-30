e6f8a385fab9b5b9e32585fec0da4e2d6c04ef9f
SheldonWong <wxt0628@gmail.com>
2018-01-30 22:00:49 +0800

    add decision tree

--- 1-LR/sklearn_LR/feature.py
+++ 1-LR/sklearn_LR/feature.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
 import pandas as pd 
+from patsy import dmatrices #根据离散变量生成哑变量
 
 train_df = pd.read_csv('E:/workpace/jupyter/machine_learning/Kaggle/Titanic/input/train.csv')
 
+'''
+#dmatrices将数据中的离散变量变成哑变量，并指明用Pclass, Sex, Embarked来预测Survived
+y, X = dmatrices('Survived~ C(Pclass) + C(Sex) + Age + C(Embarked)', data = train_df, return_type='dataframe')
+y = np.ravel(y)
 #['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp','Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
 # Name Sex Cabin Embarked 是字符串型，Cabin有缺失
+'''
+
+#ndarray
+category = pd.Categorical(train_df['Embarked'])
+print(type(category.codes))
+print(category.codes)
+
 
 '''
 # 处理缺失值
@@ -21,8 +34,10 @@ train_df['Sex_encoded'] = sex_encoder.transform(train_df['Sex'])
 # features = train_df[['Age', 'Sex_encoded', 'Pclass']]
 features = train_df[['Age', 'Sex_encoded', 'Pclass','Fare','SibSp','Parch']]
 '''
-
+'''
 labelList = ['male','female','sex']
 lb = preprocessing.LabelBinarizer()
-dummY=lb.fit_transform(labelList)
+encoder = lb.fit(labelList)
+dummY=encoder.fit_transform(labelList)
 print(dummY)
+'''
--- 1-LR/sklearn_LR/sklearn_logistic_kaggle_titanic2.py
+++ 1-LR/sklearn_LR/sklearn_logistic_kaggle_titanic2.py
@@ -11,7 +11,7 @@ test_df = pd.read_csv('E:/workpace/jupyter/machine_learning/Kaggle/Titanic/input
 
 #  Return is NOT a Numpy-matrix, rather, a Numpy-array
 # 属性查看，树数据探测，可视化，统计值等
-print(trainset_df.columns)
+print(train_df.columns)
 #['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp','Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
 # Name Sex Cabin Embarked 是字符串型，Cabin有缺失
 
@@ -21,14 +21,16 @@ train_df['Age'] = train_df['Age'].fillna(mean_age)
 
 # 特征编码
 lb = preprocessing.LabelBinarizer()
-sex_encoder = sex_encoder.fit(train_df['Sex'])
+sex_encoder = lb.fit(train_df['Sex'])
 train_df['Sex_encoded'] = sex_encoder.transform(train_df['Sex'])
 
 
 
 # 特征选择
 # features = train_df[['Age', 'Sex_encoded', 'Pclass']]
-features = train_df[['Age', 'Sex_encoded', 'Pclass','Fare','SibSp','Parch']]
+category = pd.Categorical(train_df['Embarked'])
+train_df['Embarked'] = category.codes
+features = train_df[['Age', 'Sex_encoded', 'Pclass','SibSp','Parch','Embarked']]
 
 
 
@@ -40,28 +42,31 @@ model = logreg.fit(features, labels)
 score = model.score(features, labels)
 print(score)
 
-'''
+
+
 #预测
 mean_age = test_df['Age'].mean()
 test_df['Age'] = train_df['Age'].fillna(mean_age)
 test_df['Sex_encoded'] = sex_encoder.transform(test_df['Sex'])
 
-test_features = test_df[['Age', 'Sex_encoded', 'Pclass']]
+test_category = pd.Categorical(test_df['Embarked'])
+test_df['Embarked'] = test_category.codes
+test_features = test_df[['Age', 'Sex_encoded', 'Pclass','SibSp','Parch','Embarked']]
 test_df_results = model.predict(test_features)
 
 test_df['prediction'] = test_df_results
 
 
-#expected_results_df = pd.read_csv('E:/workpace/jupyter/machine_learning/Kaggle/Titanic/input/gender_submission.csv')
-#test_df['Survived'] = expected_results_df['Survived']
+expected_results_df = pd.read_csv('E:/workpace/jupyter/machine_learning/Kaggle/Titanic/input/gender_submission.csv')
+test_df['Survived'] = expected_results_df['Survived']
 
 
 #保存结果
 result_df = pd.DataFrame()
 result_df['PassengerId'] = test_df['PassengerId']
 result_df['Survived'] = test_df['Survived']
-result_df.to_csv('result.csv',index=None)
-'''
+result_df.to_csv('result1.csv',index=None)
+
 
 '''
 X = trainset_df.as_matrix()
