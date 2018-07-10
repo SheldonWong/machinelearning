139797db5b21a0f3eadbabc5a15ea37d45a0c889
sheldonwong <sheldonwong@sheldonwongdeMacBook-Pro.local>
2018-07-10 14:58:57 +0800

    m README

--- README.m
+++ README.m
@@ -1,15 +1,15 @@
-##机器学习
+## 机器学习
 
 
-###1. LR
-###2. Linear
-###3. KNN
-###4. NaiveBayesian
-###5. SVM
-###6. DecisionTree
+### 1. LR
+### 2. Linear
+### 3. KNN
+### 4. NaiveBayesian
+### 5. SVM
+### 6. DecisionTree
       * ID3
       * C4.5
       * CART
       * GBDT
       * RandomForest
-###7. NN 
+### 7. NN 
